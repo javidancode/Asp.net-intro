3e1b266e8fd5d6a977fd9d92d602f1e74daad3eb
javidancode <javidanig@code.edu.az>
2022-10-03 04:43:22 +0400

    Added Asp.net intro

---
+++ Asp-Intro/Asp-Intro/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Asp_Intro.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        
+        }
+
+        
+    }
+}
